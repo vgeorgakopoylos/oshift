747ac7667318dc7b9d5a6b5bad0b4bd436ef930e
vgeorgakopoylos <44862839+vgeorgakopoylos@users.noreply.github.com>
2026-02-11 17:25:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def hello():
   return "Hello Oshift"
 
-if __name__ == "__main__"
+if __name__ == "__main__":
   port = int(os.environ.get("PORT", 8080))
   app.run(host="0.0.0.0", port=port)
